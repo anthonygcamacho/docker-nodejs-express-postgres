8eb53b6c5a1c9a1d9cf43cfa82d21a92dec1c89c
agcamacho <anthony.g.camacho@mail.com>
2020-06-04 23:32:45 -0700

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgUser: process.env.PGUSER,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
